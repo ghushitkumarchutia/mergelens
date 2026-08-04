789e13bbd42d9fc05a77f572d1f4a2865d1c5371
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-08-05 00:13:32 +0530

    feat(inngest): add Inngest serve route exposing reviewPullRequest and syncRepoCodebase functions

---
+++ app/api/inngest/route.ts
@@ -0,0 +1,9 @@
+import { inngest } from "@/features/inngest/client";
+import { reviewPullRequest } from "@/features/reviews/server/review-pr-function";
+import { serve } from "inngest/next";
+import { syncRepoCodebaseFunction } from "@/features/repo-sync/server/repo-sync-function";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [reviewPullRequest, syncRepoCodebaseFunction],
+});
